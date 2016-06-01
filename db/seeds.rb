6ebfe9a3f4dea44d63ea9ae51df420bac9141cb2
Aafke Smal <aafke.smal@gmail.com>
2016-06-01 18:36:50 +0200

    ja, hij is af

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.destroy_all
+
+10.times do
+  Restaurant.create(
+    name: Faker::Beer.hop,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: "chinese")
+end
